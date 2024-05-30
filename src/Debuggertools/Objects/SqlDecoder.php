e54f7a1898a02ba4de3c0c52c1b3c4341f1b5470
BISELX Charles <charles.biselx@softeam.fr>
2024-05-30 15:30:45 +0200

    upgrade sqlDecoder

--- src/Debuggertools/Objects/SqlDecoder.php
+++ src/Debuggertools/Objects/SqlDecoder.php
@@ -11,6 +11,8 @@ class SqlDecoder
         $newSql = $sql;
         $newSql = str_replace(' FROM ', " \n    FROM ", $newSql);
         $newSql = str_replace(' INNER ', " \n    INNER ", $newSql);
+        $newSql = str_replace(' ORDER ', " \n    ORDER ", $newSql);
+        $newSql = str_replace(' LIMIT ', " \n    LIMIT ", $newSql);
         return $newSql;
     }
 }
