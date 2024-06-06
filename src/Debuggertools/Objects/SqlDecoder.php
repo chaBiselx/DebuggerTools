26894470fa1a4f3b9ec122297cc2be79ba7f11ea
BISELX Charles <charles.biselx@softeam.fr>
2024-06-06 14:56:46 +0200

    Append logger

--- src/Debuggertools/Objects/SqlDecoder.php
+++ src/Debuggertools/Objects/SqlDecoder.php
@@ -10,6 +10,10 @@ class SqlDecoder
     {
         $newSql = $sql;
         $newSql = str_replace(' FROM ', " \n    FROM ", $newSql);
+        $newSql = str_replace(' CASE ', " \n    CASE ", $newSql);
+        $newSql = str_replace(' WHEN ', " \n         WHEN ", $newSql);
+        $newSql = str_replace(' ELSE ', " \n         ELSE ", $newSql);
+        $newSql = str_replace(' END ', " \n     END ", $newSql);
         $newSql = str_replace(' INNER ', " \n    INNER ", $newSql);
         $newSql = str_replace(' ORDER ', " \n    ORDER ", $newSql);
         $newSql = str_replace(' LIMIT ', " \n    LIMIT ", $newSql);
