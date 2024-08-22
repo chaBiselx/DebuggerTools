6e155f60ab2b69d6f271c972485a97305d1fa161
BISELX Charles <charles.biselx@softeam.fr>
2024-08-22 16:13:11 +0200

    UpdateSQlDecoder + tests

--- src/Debuggertools/Objects/SqlDecoder.php
+++ src/Debuggertools/Objects/SqlDecoder.php
@@ -8,6 +8,8 @@ use Debuggertools\Interfaces\SqlDecoderInterface;
 class SqlDecoder implements SqlDecoderInterface
 {
     private $indent = "    ";
+    private $SubQuery = [];
+    private $indicatorSubQuery = '!#!';
 
     public function serialize(string $sql): string
     {
@@ -29,22 +31,22 @@ class SqlDecoder implements SqlDecoderInterface
         $sql = preg_replace('/\s+/', ' ', trim($sql));
 
         // Add new lines and indentation for main SQL components
-        $sql = preg_replace('/\b(SELECT|FROM|WHERE|GROUP BY|HAVING|ORDER BY|LIMIT|OFFSET|INSERT INTO|VALUES|UPDATE|SET|DELETE FROM|INNER JOIN|LEFT JOIN|RIGHT JOIN|OUTER JOIN)\b/i', "\n$1", $sql);
+        $sql = preg_replace('/\b(FROM|WHERE|GROUP BY|HAVING|ORDER BY|LIMIT|OFFSET|INSERT INTO|VALUES|SET|INNER JOIN|LEFT JOIN|RIGHT JOIN|OUTER JOIN)\b/i', "\n$1", $sql);
         $sql = preg_replace('/\b(AND|OR)\b/i', "\n" . str_repeat($this->indent, ($level + 1)) . "$1", $sql);
-        $sql = preg_replace_callback('/\b(SELECT|FROM|WHERE|GROUP BY|HAVING|ORDER BY|LIMIT|OFFSET|INSERT INTO|VALUES|UPDATE|SET|DELETE FROM|INNER JOIN|LEFT JOIN|RIGHT JOIN|OUTER JOIN|AND|OR)\b/i', function ($matches) {
-            return strtoupper($matches[0]);
-        }, $sql);
+        $sql = $this->upperCaseSqlComponent($sql);
+
 
         // Further indentation for select fields
         if ($level === 0) {
             $sql = preg_replace('/\bSELECT\b/i', "SELECT\n" . $this->indent, $sql);
         }
-        $sql = preg_replace('/,(?![^()]*\))/', ",\n" . str_repeat($this->indent, ($level + 1)) . "", $sql);
+        $sql = $this->indentForComma($sql, $level);
 
-        // Handle subqueries by increasing the indentation level
-        $sql = preg_replace_callback('/\(\n*(SELECT(.|\n)*?)\)/i', function ($matches) use ($level) {
-            return "(" . $this->decodeString($matches[1], $level + 1) . "\n" . str_repeat($this->ident, $level) . ")";
-        }, $sql);
+        //replaces all contents in parentheses with indicators
+        $sql = $this->replaceParenthesesWithIndicator($sql);
+
+        //replaces indicators with all contents with parentheses
+        $sql = $this->replaceIndicatorWithSubQuery($sql, $level);
 
         // Trim leading and trailing whitespace from each line and add indentation for the current level
         $lines = explode("\n", $sql);
@@ -55,5 +57,84 @@ class SqlDecoder implements SqlDecoderInterface
         $formattedSql = preg_replace('/\s*$/', "", $formattedSql);
 
         return $formattedSql;
+    }
+
+    /**
+     * Upper case SQL component
+     *
+     * @param string $sql
+     * @return string
+     */
+    private function upperCaseSqlComponent(string $sql): string
+    {
+        return preg_replace_callback('/\b(SELECT|FROM|WHERE|GROUP BY|HAVING|ORDER BY|LIMIT|OFFSET|INSERT INTO|VALUES|UPDATE|SET|DELETE FROM|INNER JOIN|LEFT JOIN|RIGHT JOIN|OUTER JOIN|AND|OR)\b/i', function ($matches) {
+            return strtoupper($matches[0]);
+        }, $sql);
+    }
+
+    /**
+     * Endent after comma
+     *
+     * @param [type] $sql
+     * @param [type] $level
+     * @return string
+     */
+    private function indentForComma(string $sql, int $level): string
+    {
+        return preg_replace('/,(?![^()]*\))/', ",\n" . str_repeat($this->indent, ($level + 1)) . "", $sql);
+    }
+
+    /**
+     * replaces all contents in parentheses with indicators
+     *
+     * @param string $sql
+     * @return string
+     */
+    private function replaceParenthesesWithIndicator(string $sql): string
+    {
+        $matches = [];
+        while (preg_match('/\(([^()]+)\)/', $sql, $matches)) {
+            $index = count($this->SubQuery);
+            $this->SubQuery[$index] = $matches[1];
+            $sql = preg_replace('/\(([^()]+)\)/', $this->indicatorSubQuery . $index . $this->indicatorSubQuery, $sql, 1);
+            $matches = [];
+        }
+        return $sql;
+    }
+
+    /**
+     * replaces indicators with all contents with parentheses
+     *
+     * @param string $sql
+     * @param integer $level
+     * @return string
+     */
+    private function replaceIndicatorWithSubQuery(string $sql, int $level): string
+    {
+        $matches = [];
+        while (preg_match('/' . $this->indicatorSubQuery . '\d*' . $this->indicatorSubQuery . '/', $sql, $matches)) {
+            $index = str_replace($this->indicatorSubQuery, '', $matches[0]);
+            $subQuery = $this->SubQuery[$index];
+            $subQuery = preg_replace('/(^\(|\)$)/i', '', $subQuery); //NOSONARLINT
+
+            if (!preg_match('/\b(SELECT|UPDATE|DELETE)\b/i', $subQuery)) { // function
+                $sql = preg_replace(
+                    '/' . $this->indicatorSubQuery . '\d*' . $this->indicatorSubQuery . '/',
+                    "(" . $subQuery . ")",
+                    $sql,
+                    1
+                );
+            } else { //subquery
+                $sql = preg_replace(
+                    '/' . $this->indicatorSubQuery . '\d*' . $this->indicatorSubQuery . '/',
+                    "(\n" . $this->decodeString($subQuery, $level + 1) . "\n)",
+                    $sql,
+                    1
+                );
+            }
+            $matches = [];
+            unset($this->SubQuery[$index]);
+        }
+        return $sql;
     }
 }
--- tests/SubElement/Unit/SqlDecoderTest.php
+++ tests/SubElement/Unit/SqlDecoderTest.php
@@ -18,37 +18,43 @@ class SqlDecoderTest extends BaseTestCase
 	public function testSimpleCase()
 	{
 		$sql = "SELECT * FROM t.user";
-		$this->assertMatchesRegularExpression('/SELECT\n\s+\* \nFROM t.user$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/SELECT\n\s+\*\s*\nFROM t.user$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testUpperCase()
 	{
 		$sql = "select * from t.user";
-		$this->assertMatchesRegularExpression('/SELECT\n\s+\* \nFROM t.user$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/SELECT\n\s+\*\s*\nFROM t.user$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testSimpleCaseOverSpace()
 	{
 		$sql = "            SELECT       *         FROM         t.user           ";
-		$this->assertMatchesRegularExpression('/SELECT\n\s+\* \nFROM t.user$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/SELECT\n\s+\*\s*\nFROM t.user$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testOrderBy()
 	{
 		$sql = "SELECT * FROM t.user ORDER BY id ASC";
-		$this->assertMatchesRegularExpression('/SELECT\n\s+\* \nFROM t.user \nORDER BY id ASC$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/SELECT\n\s+\*\s*\nFROM t.user \nORDER BY id ASC$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testDelete()
 	{
 		$sql = "DELETE FROM t.user WHERE id > 10, name='toto', deptDex = 25 ";
-		$this->assertMatchesRegularExpression('/DELETE FROM t.user \nWHERE id > 10,\n\s+name=\'toto\',\n\s+deptDex = 25$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/DELETE\s*\nFROM t.user \nWHERE id > 10,\n\s+name=\'toto\',\n\s+deptDex = 25$/', $this->SqlDecoder->serialize($sql));
+	}
+
+	public function testUpdate()
+	{
+		$sql = "UPDATE t.user SET firstName = 'John',label = 'test' WHERE name='doe', deptDex = 25 ";
+		$this->assertMatchesRegularExpression('/UPDATE t.user\s*\nSET firstName = \'John\',\n\s*label = \'test\' \nWHERE name=\'doe\',\n\s*deptDex = 25$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testSimpleCaseOverSpecialChar()
 	{
 		$sql = "\n\nSELECT  \n\n     *      \n\n\n   FROM   \n\n\n      t.user   \r\n\n \t\t        ";
-		$this->assertMatchesRegularExpression('/SELECT\n\s+\* \nFROM t.user$/', $this->SqlDecoder->serialize($sql));
+		$this->assertMatchesRegularExpression('/SELECT\n\s+\*\s*\nFROM t.user$/', $this->SqlDecoder->serialize($sql));
 	}
 
 	public function testSimpleInnerJoin()
@@ -91,5 +97,11 @@ class SqlDecoderTest extends BaseTestCase
 	{
 		$sql = "SELECT sub.* FROM (SELECT * FROM tutorial.sf_crime_incidents_2014_01 WHERE day_of_week = 'Friday') sub WHERE sub.resolution = 'NONE'";
 		$this->assertMatchesRegularExpression('/SELECT\n\s*sub\.\* \nFROM \(\s*\n\s*SELECT \* \n\s*FROM tutorial\.sf_crime_incidents_2014_01 \n\s*WHERE day_of_week = \'Friday\'\s*\n\) sub \nWHERE sub\.resolution = \'NONE\'$/', $this->SqlDecoder->serialize($sql));
+	}
+
+	public function testComplexe1()
+	{
+		$sql = "SELECT LEFT(sub.date, 2) AS cleaned_month,sub.day_of_week,AVG(sub.incidents) AS average_incidents FROM (SELECT day_of_week,date,COUNT(incidnt_num) AS incidents FROM tutorial.sf_crime_incidents_2014_01 GROUP BY 1,2) sub GROUP BY 1,2 ORDER BY 1,2";
+		$this->assertMatchesRegularExpression('/SELECT\n\s*LEFT\(sub\.date, 2\) AS cleaned_month,\n\s*sub.day_of_week,\n\s*AVG\(sub\.incidents\) AS average_incidents\s*\nFROM \(\s*\n\s*SELECT day_of_week,\n\s*date,\n\s*COUNT\(incidnt_num\) AS incidents\s*\n\s*FROM tutorial\.sf_crime_incidents_2014_01\s*\n\s*GROUP BY 1,\s*\n\s*2\s*\n\s*\) sub \nGROUP BY 1,\n\s*2 \sORDER BY 1,\n\s*2$/', $this->SqlDecoder->serialize($sql));
 	}
 }
